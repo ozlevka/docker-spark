b7d647ced100ae994c7cc9b8a8da238fbf9927d0
ozlevka <ozlevka@gmail.com>
2020-06-27 13:07:46 +0300

    I can develop in jupyter and spark

--- build.sh
+++ build.sh
@@ -2,21 +2,31 @@
 
 set -e
 
-TAG=2.4.5-hadoop2.7
+if [[ -n "$1" ]]; then
+    TAG="$1"
+else
+    TAG=$(date +%Y%m%d%H%M)
+fi
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
-    cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
+    DIR=$NAME
+    if [[ "$DIR" = "dev" ]]; then
+        DIR=".devcontainer"
+    fi
+    IMAGE=ozlevka/spark-$NAME:$TAG
+    cd $([ -z "$2" ] && echo "./$DIR" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
+    docker push $IMAGE
     cd -
 }
 
+build dev
 build base
 build master
 build worker
-build submit
-build java-template template/java
-build scala-template template/scala
-build python-template template/python
+#build submit
+#build java-template template/java
+#build scala-template template/scala
+#build python-template template/python
