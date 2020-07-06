aa33e0a9da620449a929171f862acd7f8e8a6b26
Lev <lev@Levs-MBP.lan>
2020-07-06 12:10:23 +0300

    tmp saving

--- master/master.sh
+++ master/master.sh
@@ -12,5 +12,13 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
+mkdir -p /tmp/spark-events
+
+export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS -Dspark.history.fs.logDirectory=file:/tmp/spark-events"
+
+/spark/sbin/start-history-server.sh
+
 cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
     --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+
+
