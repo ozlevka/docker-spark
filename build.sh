20e0977c2b6d17789a32b8fcbb3f081ecf70fdfc
Lev <lev@Levs-MBP.lan>
2020-07-20 16:44:55 +0300

    Ignore Jupyter checkpoints And spark 3 on debian

--- build.sh
+++ build.sh
@@ -2,21 +2,22 @@
 
 set -e
 
-TAG=2.4.5-hadoop2.7
+TAG=3.0-debian
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=ozlevka/spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
+    docker push $IMAGE
     cd -
 }
 
 build base
 build master
 build worker
-build submit
-build java-template template/java
-build scala-template template/scala
-build python-template template/python
+# build submit
+# build java-template template/java
+# build scala-template template/scala
+# build python-template template/python
