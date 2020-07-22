3cf61e8afe117ebe758de4d1f5fbb47fb1217534
Lev <lev@Levs-MacBook-Pro.local>
2020-07-22 07:58:43 +0300

    Change command and build for docker-compose

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-TAG=2.4.5-hadoop2.7
+TAG=2.2.0
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=bde2020/spark-$NAME-debian:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
