578f79c5ee24dce74afa99c755ab0cae7a950173
Lev <lev@Levs-MBP.lan>
2020-07-29 20:47:10 +0300

    Save changes in history start script

--- history/history.sh
+++ history/history.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export SPARK_DAEMON_MEMORY="24g"
-export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS -Dfs.s3.maxConnections=100 -Dfs.s3a.connection.timeout=50000 -Dspark.history.fs.numReplayThreads=45 -Dspark.history.retainedApplications=100  -Dspark.history.fs.logDirectory=s3a://ww-standalone/spark-dev/logs/"
+#export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS -Dfs.s3.maxConnections=100 -Dfs.s3a.connection.timeout=50000 -Dspark.history.fs.numReplayThreads=45 -Dspark.history.retainedApplications=100  -Dspark.history.fs.logDirectory=s3a://ww-standalone/spark-dev/logs/"
+export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS -Dspark.history.fs.logDirectory=s3a://ww-standalone/spark-dev/logs/ -Dspark.history.store.path=/data"
 /spark/sbin/start-history-server.sh
 while sleep 1000; do :; done
